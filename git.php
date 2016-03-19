aba3b7a43e0b655f7fa96942fdeae3565185ac95
diegargon <kn@envigo.net>
2016-03-19 13:20:56 +0100

    Standard Commit

--- git.php
+++ git.php
@@ -1,49 +1,43 @@
 <!--
  Copyright @ 2016 Diego Garcia
+ V:0.001
 -->
 <?php
-require "config/config.php";
+require "config/gittoken.conf.php"; // <-- $token = '26cd28613f4b503c745c83bcc011bac73acda5e6'; 
 
+require "include/git.inc.php";
 
-//$token = '26cd28613f4b503c745c83bcc011bac73acda5e6';
-//
 //User Config
 $user = 'diegargon';
-
-
-
 //End User Config
-
-function get_readme($reponame) {
-  $curl_readme_url = 'https://api.github.com/repos/' . $GLOBALS["user"] . '/' . $reponame .'/readme';
-  $response = curl_get($curl_readme_url);
-  return base64_decode($response->content);
-}
-
-function get_repo() {
-    $curl_repo_url = 'https://api.github.com/users/' . $GLOBALS["user"] . '/repos';
-    $response = curl_get($curl_repo_url);  
-    //print_r($response);
-    return $response;
-}
-
-function curl_get($url){
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: Awesome-Octocat-App', $GLOBALS["curl_token"]));
-    $response = curl_exec($ch);
-    curl_close($ch);
-    return json_decode($response);  
-}
-
+ 
 ?>
 <div class="blackdiv"></div>
-<div class="wrapper_w100_padding">
-
-    <div class="github-card"  data-user="diegargon"></div>
-    <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
-
-
+<div class="wrapper_w100_padding"> 
+    <div class="github_user">
+    <?php
+        $user_info = get_user($user);
+        echo "<div class='github_user_top'>";
+        echo "<div class='github_user_top_avatar'><img src='$user_info->avatar_url'></div>";
+        echo "<div class='github_user_top_name'><a href='$user_info->html_url'>$user_info->name</a><br/>@$user_info->login</div>";
+        echo "</div>";
+        
+        echo "<div class='github_user_middle_details'>";
+        echo "<ul>";
+        echo "<li>Company: $user_info->company</li>";
+        echo "<li>Location: $user_info->location</li>";
+        echo "<li>Web: <a href='$user_info->blog'>$user_info->blog</a></li>";
+        echo "<li>Email:<a href='mailto:$user_info->email'>$user_info->email</a></li>";        
+        echo "</ul>";
+        echo "</div>";
+        
+        echo "<div class='github_user_bottom_details'>";
+        echo "<a  href='$user_info->repos_url'><strong>$user_info->public_repos</strong><span>Repos</span></a>";
+        echo "<a  href='$user_info->gist_url'><strong>$user_info->public_gists</strong><span>Gist</span></a>";
+        echo "<a  href='$user_info->followers_url'><strong>$user_info->followers</strong><span>Followers</span></a>";
+        echo "</div>";
+    ?>
+    </div>
     
     <div class="github_repo">
     
@@ -51,14 +45,20 @@ function curl_get($url){
 
 $curl_token = 'Authorization: token ' . $token;
 
-$response = get_repo();
+$response = get_repos();
 
   if (!empty($response)) {
     
     foreach ($response as $repo) {
       $readme_content = get_readme($repo->name);
-      print '<a href="' . $repo->html_url . '" target="_blank">' . $repo->name . '</a><br />';      
+      print '<b>Project</b>: <a href="' . $repo->html_url . '" target="_blank">' . $repo->name . '</a><br />';      
+      print '<div class="github_readme">';
+      print '<p>Readme:</p>';
       echo nl2br(htmlspecialchars($readme_content));
+      print '<br/>';
+      print '</div>';
+      print '<br/>';
+      
     }
   }
 ?>
---
+++ include/git.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+/* 
+ *  Copyright @ 2016 Diego Garcia
+ */
+
+if($_SERVER['PHP_SELF'] == '/' . basename(__FILE__)) {
+        exit();
+}
+
+function get_user($user) {
+    $curl_user_url = 'https://api.github.com/users/' . $GLOBALS["user"] . '';
+    return curl_Get($curl_user_url);
+}
+
+function get_readme($reponame) {
+  $curl_readme_url = 'https://api.github.com/repos/' . $GLOBALS["user"] . '/' . $reponame .'/readme';
+  $response = curl_get($curl_readme_url);
+  return base64_decode($response->content);
+}
+
+function get_repos() {
+    $curl_repo_url = 'https://api.github.com/users/' . $GLOBALS["user"] . '/repos';
+    return curl_get($curl_repo_url);  
+    
+    //return $response; 
+}
+
+function curl_get($url){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: Awesome-Octocat-App', $GLOBALS["curl_token"]));
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($response);  
+}
+
